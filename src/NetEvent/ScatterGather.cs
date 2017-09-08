f9a97f1d565f8eaf0e6fe591e542ea05114e42c7
rogeralsing <rogeralsing@gmail.com>
2017-09-08 19:57:55 +0200

    first commit

---
+++ src/NetEvent/ScatterGather.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetEvent
+{
+    public static class ScatterGather<TRequest, TResponse>
+    {
+        internal static readonly ConcurrentBag<Func<TRequest, Task<TResponse>>> Subscriptions = new ConcurrentBag<Func<TRequest, Task<TResponse>>>();
+        public static void SubscribeAsync(Func<TRequest, Task<TResponse>> subscription)
+        {
+            Subscriptions.Add(subscription);
+        }
+        public static void RegisterHandler(Func<TRequest, TResponse> subscription)
+        {
+            Task<TResponse> Wrapper(TRequest m) => Task.FromResult(subscription(m));
+            Subscriptions.Add(Wrapper);
+        }
+
+        public static Task<TResponse[]> Request(TRequest request, TimeSpan timeout)
+        {
+            var cts = new CancellationTokenSource(timeout);
+            return Request(request, cts.Token);
+        }
+
+        public static async Task<TResponse[]> Request(TRequest request, CancellationToken ct)
+        {
+            var l = Subscriptions.Select(sub => sub(request)).ToList();
+
+            var all = Task.WhenAll(l);
+            await Task.WhenAny(all, Task.Delay(Timeout.Infinite, ct));
+            var results = l.Where(t => t.Status == TaskStatus.RanToCompletion).Select(t => t.Result).ToArray();
+            return results;
+        }
+    }
+}
